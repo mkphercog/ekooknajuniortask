19871754bc89011a4dd64c6ed2c18964b4932d2e
Marcin H <mkp.hercog@gmail.com>
2022-07-23 13:19:45 +0200

    separate User from UserList

--- src/components/UsersList/UsersList.js
+++ src/components/UsersList/UsersList.js
@@ -1,32 +1,17 @@
-import React, { useContext } from "react";
-import { ContextStorage } from "../../common/ContextStorage";
+import React from "react";
+import { User } from "./../User/User";
 import "./UsersList.scss";
 
-export const UsersList = () => {
-  const { usersData } = useContext(ContextStorage);
-  const renderUsers = usersData.map((user) => (
-    <li key={user.id} className="list__user">
-      <p className="list__user_details">{`${user.first_name} ${user.last_name}`}</p>
-      <p className="list__user_details">{user.age}</p>
-      <p className="list__user_details">
-        {`${user.postal_code} ${user.city}, ul. ${user.street}`}
-      </p>
-      <div className="list__user_details">
-        <button className="list__user_btn">Aktualizuj</button>
-        <button className="list__user_btn">Usuń</button>
-      </div>
-    </li>
-  ));
-
-  return (
-    <div className="list">
-      <div className="list__header">
-        <p className="list__header_description">Imię i nazwisko</p>
-        <p className="list__header_description">Wiek</p>
-        <p className="list__header_description">Adres zamieszkania</p>
-        <p className="list__header_description">Działania</p>
-      </div>
-      <ul className="list__users_list">{renderUsers}</ul>
+export const UsersList = () => (
+  <div className="list">
+    <div className="list__header">
+      <p className="list__header_description">Imię i nazwisko</p>
+      <p className="list__header_description">Wiek</p>
+      <p className="list__header_description">Adres zamieszkania</p>
+      <p className="list__header_description">Działania</p>
     </div>
-  );
-};
+    <ul className="list__users_list">
+      <User />
+    </ul>
+  </div>
+);
